4fd197066505f6f745e39c9a12ce1c33ed7ec0f6
ramon <ramonezorrilla@gmail.com>
2024-06-06 15:45:07 -0400

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el número para comenzar la serie Fibonacci:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Serie Fibonacci:");
+        for (int i = 0; i < numero; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
